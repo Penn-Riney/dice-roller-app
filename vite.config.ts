b27f4f7c315085ba03f3ef194b77c4a8a9c26461
pennriney <jriney1@jh.edu>
2026-05-10 13:46:38 -0400

    Fix Vite base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/https://github.com/Penn-Riney/dice-roller-app/",
+  base: "/dice-roller-app/",
   plugins: [react(), tailwindcss()],
 });
